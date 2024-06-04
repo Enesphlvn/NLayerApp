d1b66c2fcc171d3636a65f2460ab085da7f953bf
Enesphlvn <pehlivan.enes53@gmail.com>
2024-06-04 18:18:00 +0300

    Dto objects created

---
+++ NLayer.Core/DTOs/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NLayer.Core.DTOs
+{
+    public class ProductDto : BaseDto
+    {
+        public string Name { get; set; } = null!;
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
