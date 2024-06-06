430b26414101d6143de226387e76d285dbe0a153
Enesphlvn <pehlivan.enes53@gmail.com>
2024-06-06 16:30:27 +0300

    CustomResponseDto class created

--- NLayer.Core/DTOs/ProductFeatureDto.cs
+++ NLayer.Core/DTOs/ProductFeatureDto.cs
@@ -2,7 +2,6 @@
 {
     public class ProductFeatureDto : BaseDto
     {
-        public int Id { get; set; }
         public string? Color { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
